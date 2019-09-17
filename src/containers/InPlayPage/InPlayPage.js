762da7dabde42fac7ddd300edd9e063a93949c67
JellyDev <jellydev@MacBook-Pro-Leonid.local>
2019-09-17 16:36:33 +0300

    created v1 template of in play page

---
+++ src/containers/InPlayPage/InPlayPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const InPlayPage = props => {
+  return <div>InPlayPage</div>;
+};
+
+InPlayPage.propTypes = {};
+
+export default InPlayPage;
