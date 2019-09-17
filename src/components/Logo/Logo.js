8846350de943c66312231ad6dc34124c85bd5653
JellyDev <jellydev@MacBook-Pro-Leonid.local>
2019-09-17 17:33:39 +0300

    added link to home page on logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Logo.scss';
 
 const Logo = props => {
-  return <div className="logo">LOGO</div>;
+  return (
+    <div className="logo">
+      <Link to="/">LOGO</Link>
+    </div>
+  );
 };
 
 export default Logo;
