782dc426311da2e04f1b502688b5680c9053df69
JellyDev <jellydev@MacBook-Pro-Leonid.local>
2019-09-18 14:43:13 +0300

    created template of in-play table

--- src/components/InPlayTable/InPlayTable.js
+++ src/components/InPlayTable/InPlayTable.js
@@ -1,10 +1,196 @@
 import React from 'react';
+import { withTranslation } from 'react-i18next';
+import BetItem from '../BetItem';
 import './InPlayTable.scss';
 
-const Header = props => {};
+import soccer from '../../images/soccer-ball-variant.svg';
+import uefa from '../../images/european_flag.svg';
 
-const InPlayTable = () => {
-  return <section className="in-play-table">{/* InPlayTable */}</section>;
+import mu from '../../images/manchester.png';
+import ju from '../../images/juventus.png';
+
+const Header = ({ text }) => {
+  return (
+    <header className="flex table-header">
+      <div className="col time">{text.time}</div>
+      <div className="col play">{text.play}</div>
+      {text.markets.map((item, index) => {
+        return (
+          <div key={index} className="col market">
+            <div className="type">{item.type}</div>
+            <div className="flex odds">
+              {item.odds.map((odd, idx) => {
+                return (
+                  <div key={idx} className="odd">
+                    {odd}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+      <div className="col more"></div>
+      <div className="col favourite"></div>
+    </header>
+  );
 };
 
-export default InPlayTable;
+const Game = props => {
+  return (
+    <div className="game">
+      <div className="flex row">
+        <div className="col time">
+          <div className="score">1:0</div>
+          <div className="time">16''34'</div>
+        </div>
+        <div className="col play">
+          <div className="flex">
+            <img src={mu} />
+            <span>Manchester United</span>
+          </div>
+          <div className="flex">
+            <img src={ju} />
+            <span>Manchester United</span>
+          </div>
+          <div className="flex draw">Draw</div>
+        </div>
+        <div className="col market">
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+        </div>
+        <div className="col market">
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+        </div>
+        <div className="col market">
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+        </div>
+        <div className="col market"></div>
+        <div className="col market"></div>
+        <div className="col market"></div>
+        <div className="col mores">+8</div>
+        <div className="col favourite">
+          <i className="far fa-star"></i>
+        </div>
+      </div>
+      <div className="flex row">
+        <div className="col time"></div>
+        <div className="col play"></div>
+        <div className="col market">
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+        </div>
+        <div className="col market">
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+        </div>
+        <div className="col market"></div>
+        <div className="col market"></div>
+        <div className="col market"></div>
+        <div className="col market"></div>
+        <div className="col mores"></div>
+        <div className="col favourite"></div>
+      </div>
+      <div className="flex row">
+        <div className="col time"></div>
+        <div className="col play"></div>
+        <div className="col market">
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+        </div>
+        <div className="col market">
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+          <div className="flex odd">
+            <span>+0.75</span>
+            <BetItem>1.89</BetItem>
+          </div>
+        </div>
+        <div className="col market"></div>
+        <div className="col market"></div>
+        <div className="col market"></div>
+        <div className="col market"></div>
+        <div className="col mores"></div>
+        <div className="col favourite"></div>
+      </div>
+    </div>
+  );
+};
+
+const League = props => {
+  return (
+    <div className="league">
+      <div className="flex soccer name">
+        <img src={soccer} alt="sport-icon" />
+        <img src={uefa} alt="league-icon" />
+        <span className="text">Soccer - UEFA Champions League</span>
+      </div>
+      <div className="games-list">
+        {[1, 2].map((item, index) => {
+          return <Game key={index} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+const InPlayTable = ({ t }) => {
+  const text = t('inPlayTable', { returnObjects: true });
+
+  return (
+    <section className="in-play-table">
+      <Header text={text.header} />
+      <div className="leagues-list">
+        {[2, 1].map((item, index) => {
+          return <League key={index} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default withTranslation()(InPlayTable);
